d0e07292835085f23bfcd9b2a4bd946cd8fe1a8a
skersas <ignaxtas@gmail.com>
2024-10-08 15:12:36 +0300

    initial commit

---
+++ projektas_streamlit/main.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import pandas as pd
+from dataclasses import dataclass
+from typing import Literal
+ 
+st.write("""
+Hello *world!*
+""")
+
+
+import streamlit as st
+from dataclasses import dataclass
+from typing import Literal
+ 
+if 'irasai' not in st.session_state:
+    st.session_state["irasai"] = []
+ 
+irasai = st.session_state["irasai"]
+ 
+@dataclass
+class Record:
+    value: float
+    aprasymas: str
+    tipas: Literal["islaidos", "pajamos"]
+ 
+col1, col2, col3, col4 = st.columns(4)
+with col4:
+    pressed = st.button("Submit")
+ 
+with col1:
+    value = st.number_input("Money value", 0.0, 10_000.0, step=0.01)
+ 
+with col2:
+    description = st.text_input("Description")
+ 
+with col3:
+    record_type = st.selectbox("Type", ["islaidos", "pajamos"])
+ 
+if pressed:
+    irasai.append(Record(value, description, record_type))
+ 
+islaidos, pajamos = [], []
+balansas = 0
+for irasas in irasai:
+    if irasas.tipas == "islaidos":
+        islaidos.append(irasas)
+        balansas -= irasas.value
+    if irasas.tipas == "pajamos":
+        pajamos.append(irasas)
+        balansas += irasas.value
+ 
+with st.expander("Islaidos"):
+    for irasas in islaidos:
+        st.markdown(f":red[{irasas.tipas}]: {irasas.value} for {irasas.aprasymas}")
+ 
+with st.expander("Pajamos"):
+    for irasas in pajamos:
+        st.markdown(f":green[{irasas.tipas}]: {irasas.value} for {irasas.aprasymas}")
+ 
+if balansas > 0:
+    st.markdown(f"**Balansas**: :green[{balansas}]")
+elif balansas < 0:
+    st.markdown(f"**Balansas**: :red[{balansas}]")
+
+
+
+data = [70000, 50000, 60000, 80000, 45000, 10000, 5000, 3000, 50000, 65000]
+
+ 
+df = pd.DataFrame(data)
+st.line_chart(df)
